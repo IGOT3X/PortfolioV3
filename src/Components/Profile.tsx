89d0571b3efd3f63b6f067ad434cbaf60819fd7b
Luka Igrutinovic <lukaigrutinovic@icloud.com>
2023-07-23 20:06:32 +0200

    General work

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -3,19 +3,24 @@ import { useState } from "react";
 const Profile = ()=>{
 
     const [toolTip,setToolTip] = useState("Luka Igrutinović");
+    const [bSpinImage,setbSpinImage] =useState(false);
 
     const ResetToolTip = ()=>{
         setToolTip("Luka Igrutinović");
     }
 
+    const SwitchImageSpin = ()=>{
+        setbSpinImage(!bSpinImage);
+    }
+
     return(
         <div className="flex items-center gap-[23px]">
-        <img className="w-[64px] h-[64px]" src="pfp.png" alt="" />
+        <img onClick={SwitchImageSpin} className={`w-[64px] h-[64px] transition ease-in-out duration-300 cursor-pointer ${bSpinImage?"sm:animate-spin":"hover:scale-105"}`} src="pfp.png" alt="" />
         <div className="relative">
             <h1 className="font-semibold text-[24px] w-[183px]">{toolTip}</h1>
-            <div className="absolute flex gap-[8px] top-[-16px] right-[-80px]">
-                <a onMouseOver={()=>setToolTip("IGot3X")} onMouseLeave={ResetToolTip} href="https://github.com/IGOT3X" target="_blank"><img src="github.svg" alt="" /></a>
-                <a onMouseOver={()=>setToolTip("Download CV")} onMouseLeave={ResetToolTip} href="#"><img src="cv.svg" alt="" /></a>
+            <div className="sm:absolute flex gap-[8px] sm:top-[-16px] sm:right-[-80px] justify-center">
+                <a onMouseOver={()=>setToolTip("GitHub")} onMouseLeave={ResetToolTip} href="https://github.com/IGOT3X" className="transition ease-in-out duration-300 hover:scale-105" target="_blank"><img src="github.svg" alt="" /></a>
+                <a onMouseOver={()=>setToolTip("Download CV")} onMouseLeave={ResetToolTip} href="#" className="transition ease-in-out duration-300 hover:scale-105"><img src="cv.svg" alt="" /></a>
             </div>
         </div>
     </div>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,14 +5,14 @@ const Home = () => {
         <div className="w-screen sm:h-screen bg-black-900 sm:flex text-black-100">
             <div className="absolute sm:w-[1200px] sm:h-[1200px] rounded-full radial-bubble top-[-600px] left-[-200px]"></div>
             <div className="absolute sm:w-[1200px] sm:h-[1200px] rounded-full radial-bubble bottom-[-600px] right-[-200px]"></div>
-            <div className="sm:ml-[123px] pt-[64px] sm:pt-0 w-full h-full flex flex-col items-start justify-center sm:max-w-[513px] z-40">
+            <div className="sm:ml-[123px] pt-[64px] sm:pt-0 w-full h-full flex flex-col sm:items-start items-center justify-center sm:max-w-[513px] z-40 fade-in">
                 <Profile />
-                <h1 className="font-semibold sm:text-[96px] text-[64px]">Full-Stack <br /> developer;</h1>
-                <p className="text-green-300 font-semibold sm:text-[24px] pt-[8px]">Proficient in crafting dynamic and user-centric web applications.</p>
-                <div className="w-[130px] h-[60px] mt-[17px]">
+                <h1 className="font-semibold sm:text-[96px] text-[64px] pt-[24px]">Full-Stack <br /> developer;</h1>
+                <p className="text-green-300 font-semibold sm:text-[24px] pt-[8px] text-center sm:text-left px-[16px] sm:px-0">Proficient in crafting dynamic and user-centric web applications.</p>
+                <div className="w-[130px] h-[60px] mt-[17px] items-center justify-center flex sm:block">
                     <a href="#" className="p-[10px] bg-black-100 text-[18px] font-semibold text-black-900 rounded-[8px] transition-[padding] ease-in-out duration-300 hover:p-[12px]">Projects</a>
                 </div>
-                <div className="flex flex-col items-center mx-auto gap-[17px]">
+                <div className="flex flex-col items-center mx-auto gap-[17px] pt-[24px] sm:pt-0">
                     <p className="text-black-400 font-semibold">Technology stack:</p>
                     <div className="flex flex-wrap max-w-[294px] sm:max-w-full justify-center gap-[16px]">
                         <img src="figma.svg" alt="" />
@@ -27,7 +27,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className="md:flex hidden flex-col transition ease-in-out duration-700 2xl:rotate-[-32deg] xl:rotate-[-24deg] sm:rotate-[0deg] items-center gap-[75px]">
+            <div className="md:flex hidden sm:overflow-auto xl:overflow-visible flex-col transition ease-in-out duration-700 2xl:rotate-[-32deg] xl:rotate-[-24deg] sm:rotate-[0deg] items-center gap-[75px] fade-in">
                 <img className="w-[720px]" src="unknown-big.png" alt="" />
                 <img className="w-[1018px]" src="vf-big.png" alt="" />
                 <img className="w-[720px]" src="tech-big.png" alt="" />
